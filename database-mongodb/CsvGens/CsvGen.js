1893ee313f4208629006efee5f9779739cce2983
vargas055 <juanjo220393@gmail.com>
2020-10-16 20:13:13 -0400

    Implemented data generation script and reformated read me

---
+++ database-mongodb/CsvGens/CsvGen.js
@@ -0,0 +1,268 @@
+console.log('we writing boyzzzz......')
+const begTime = new Date()
+const fs = require('fs');
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function randomBool(){
+    let boolArr = [true, false];
+    return boolArr[getRandomInt(0, 1)]
+}
+
+
+const writeUsers = fs.createWriteStream('10MillionRecords!.csv');
+writeUsers.write('id,name,category,price,rating,imageUrl,onSale\n', 'utf8');
+
+function writeTenMillionUsers(writer, encoding, callback) {
+    let i = 10000000;
+    let id = 0;
+    function write() {
+      let ok = true;
+      do {
+        i -= 1;
+        id += 1;
+        function dataMult (num) {
+            let result;
+            for (var a = 0; a < num; a++ ) {
+                if (!result) {
+                    result = `${id},${fakeNameGen()},${fakeProdGen()},${getRandomInt(20, 300)},${(Math.random() * (5 - 1 + 1) + 1).toFixed(2)},${`https://unsplash.it/500/300?image=${getRandomInt(0, 1000)}`},${randomBool()}\n`
+                } else {
+                    result +=`${id},${fakeNameGen()},${fakeProdGen()},${getRandomInt(20, 300)},${(Math.random() * (5 - 1 + 1) + 1).toFixed(2)},${`https://unsplash.it/500/300?image=${getRandomInt(0, 1000)}`},${randomBool()}\n`
+                }
+            }
+            return result
+        }
+        let data = `${dataMult(10)}`;
+        if (i === 0) {
+          writer.write(data, encoding, callback);
+        } else {
+          ok = writer.write(data, encoding);
+        }
+      } while (i > 0 && ok);
+      if (i > 0) {
+        writer.once('drain', write);
+      }
+    }
+  write()
+  }
+
+  writeTenMillionUsers(writeUsers, 'utf-8', () => {
+    writeUsers.end();
+    let endTime = new Date()
+    console.log('Ayyyyy we did it! it took aproximately', ((endTime - begTime)/60000).toFixed(2), 'mins')
+  });
+
+
+
+function fakeNameGen() {
+    let arr = ["Unbranded Metal Bacon",
+    "Incredible Concrete Hat",
+    "Fantastic Concrete Shirt",
+    "Handcrafted Soft Shirt",
+    "Refined Wooden Sausages",
+    "Awesome Steel Keyboard",
+    "Sleek Frozen Ball",
+    "Refined Wooden Towels",
+    "Handcrafted Metal Towels",
+    "Small Fresh Tuna",
+    "Handmade Steel Cheese",
+    "Refined Rubber Table",
+    "Handmade Wooden Mouse",
+    "Generic Wooden Pants",
+    "Fantastic Concrete Hat",
+    "Licensed Granite Table",
+    "Handcrafted Cotton Hat",
+    "Ergonomic Frozen Hat",
+    "Sleek Soft Pants",
+    "Handmade Cotton Keyboard",
+    "Handcrafted Wooden Computer",
+    "Incredible Granite Keyboard",
+    "Unbranded Plastic Bacon",
+    "Licensed Cotton Car",
+    "Ergonomic Metal Salad",
+    "Handmade Plastic Ball",
+    "Licensed Metal Table",
+    "Awesome Frozen Soap",
+    "Licensed Cotton Pizza",
+    "Tasty Concrete Chips",
+    "Rustic Metal Hat",
+    "Intelligent Concrete Shoes",
+    "Ergonomic Fresh Ball",
+    "Fantastic Frozen Shoes",
+    "Fantastic Metal Hat",
+    "Ergonomic Frozen Hat",
+    "Incredible Frozen Soap",
+    "Refined Frozen Table",
+    "Incredible Soft Soap",
+    "Practical Steel Hat",
+    "Intelligent Metal Computer",
+    "Unbranded Rubber Bike",
+    "Fantastic Soft Sausages",
+    "Refined Soft Pants",
+    "Handmade Concrete Keyboard",
+    "Handcrafted Granite Cheese",
+    "Rustic Rubber Keyboard",
+    "Refined Rubber Computer",
+    "Licensed Soft Soap",
+    "Intelligent Wooden Mouse",
+    "Sleek Rubber Towels",
+    "Refined Steel Car",
+    "Rustic Cotton Towels",
+    "Handmade Rubber Soap",
+    "Refined Concrete Computer",
+    "Gorgeous Concrete Chips",
+    "Refined Metal Tuna",
+    "Gorgeous Fresh Bike",
+    "Licensed Plastic Chair",
+    "Licensed Plastic Tuna",
+    "Practical Fresh Keyboard",
+    "Rustic Soft Tuna",
+    "Handcrafted Plastic Pants",
+    "Gorgeous Plastic Bike",
+    "Ergonomic Granite Pizza",
+    "Licensed Concrete Salad",
+    "Handmade Metal Hat",
+    "Practical Soft Tuna",
+    "Incredible Frozen Keyboard",
+    "Licensed Rubber Soap",
+    "Tasty Fresh Bike",
+    "Practical Wooden Shoes",
+    "Intelligent Rubber Pants",
+    "Small Concrete Cheese",
+    "Fantastic Soft Fish",
+    "Practical Metal Hat",
+    "Fantastic Soft Salad",
+    "Sleek Metal Shirt",
+    "Handmade Fresh Chair",
+    "Ergonomic Soft Soap",
+    "Handcrafted Granite Gloves",
+    "Unbranded Concrete Mouse",
+    "Licensed Plastic Bike",
+    "Rustic Cotton Mouse",
+    "Tasty Plastic Gloves",
+    "Licensed Soft Cheese",
+    "Tasty Concrete Fish",
+    "Refined Frozen Gloves",
+    "Licensed Granite Computer",
+    "Handmade Soft Gloves",
+    "Tasty Rubber Ball",
+    "Handcrafted Fresh Towels",
+    "Fantastic Cotton Bacon",
+    "Incredible Fresh Chips",
+    "Tasty Rubber Ball",
+    "Awesome Metal Car",
+    "Unbranded Frozen Shoes",
+    "Ergonomic Concrete Hat",
+    "Intelligent Concrete Ball",
+    "Rustic Cotton Table",
+    ]
+    
+    return arr[getRandomInt(0, 100)]
+}
+
+function fakeProdGen() {
+    let arr = ["Fish",
+        "Towels",
+        "Bike",
+        "Pizza",
+        "Towels",
+        "Pizza",
+        "Salad",
+        "Hat",
+        "Gloves",
+        "Tuna",
+        "Table",
+        "Tuna",
+        "Computer",
+        "Shoes",
+        "Hat",
+        "Towels",
+        "Mouse",
+        "Table",
+        "Computer",
+        "Pants",
+        "Keyboard",
+        "Chips",
+        "Fish",
+        "Computer",
+        "Cheese",
+        "Ball",
+        "Shirt",
+        "Mouse",
+        "Pizza",
+        "Car",
+        "Table",
+        "Mouse",
+        "Car",
+        "Chips",
+        "Shoes",
+        "Chair",
+        "Keyboard",
+        "Cheese",
+        "Chair",
+        "Soap",
+        "Tuna",
+        "Bacon",
+        "Bike",
+        "Shoes",
+        "Soap",
+        "Salad",
+        "Bacon",
+        "Table",
+        "Ball",
+        "Towels",
+        "Hat",
+        "Cheese",
+        "Fish",
+        "Salad",
+        "Keyboard",
+        "Car",
+        "Fish",
+        "Bacon",
+        "Towels",
+        "Table",
+        "Hat",
+        "Bacon",
+        "Keyboard",
+        "Shirt",
+        "Bike",
+        "Bike",
+        "Sausages",
+        "Sausages",
+        "Hat",
+        "Fish",
+        "Tuna",
+        "Shirt",
+        "Keyboard",
+        "Gloves",
+        "Computer",
+        "Mouse",
+        "Bike",
+        "Salad",
+        "Chair",
+        "Towels",
+        "Chicken",
+        "Computer",
+        "Mouse",
+        "Shirt",
+        "Salad",
+        "Table",
+        "Chair",
+        "Pizza",
+        "Fish",
+        "Bike",
+        "Towels",
+        "Fish",
+        "Table",
+        "Bacon",
+        "Hat",
+        "Bike",
+        "Shoes",
+        "Bacon",
+        "Fish",
+        "Hat",
+        ]
+    
+    return arr[getRandomInt(0, 100)]
+}
--- database-mongodb/seed.js
+++ database-mongodb/seed.js
@@ -1,49 +1,25 @@
 const db = require('./index.js');
-const faker = require('faker');
 const Furniture = require('./Furniture.js');
-const coolImages = require('cool-images');
 const mongoose = require('mongoose')
 
-//use faker.js to generate random data
-//create 100 copies of sample furniture
-const furnitureArr = [];
-let begTime = new Date()
-const bulkCreate = () => {
-    // for (let a = 0; a < 100; a++) {
 
-        for (let i = 0; i < 10000; i++) {
-            for (let j = 0; j < 10; j++) {
-                let sampleFurniture = {
-                    id: i,
-                    name: faker.commerce.productName(),
-                    category: faker.commerce.product(),
-                    price: faker.random.number({min: 20, max: 300}),
-                    rating: faker.random.number({min: 1, max: 5, precision: 0.5}),
-                    imageUrl: coolImages.one(),
-                    onSale: faker.random.boolean()
-                };
-                furnitureArr.push(sampleFurniture);
-            }
-        }
-    // }
-};
-
-bulkCreate();
-
-//store the 100 copies of sample furnitures
 Furniture.deleteMany()
 .then(res => {
-    console.log('deleted:', res.deletedCount);
+    console.log('deleted:', res.deletedCount, 'records!');
 })
 .then(()=>{
     Furniture.insertMany(furnitureArr, (error, data) => {
         if (error) {
-            console.log(error);
+            console.log('error:', error);
         } else {
             let endTime = new Date()
-            console.log('Successfully stored:', data.length, 'items into the database and it took:', (endTime - begTime), 'miliseconds')
+            console.log('Successfully stored:', data.length, 'items into the database and it took:', ((endTime - begTime)/60000).toFixed(2), 'mins')
             mongoose.disconnect();
         }
-    });
+    }) 
+})
+.catch(err => {
+    console.log('there was an error!', err)
+    mongoose.disconnect();
 })
 
